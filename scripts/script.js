5095d55e026f11fd4148acefbc2d5caecb5d0ac5
Rávilon <ravilon.ads@inf.ufpel.edu.br>
2023-12-03 20:19:52 -0300

    Commiting some changes and a prank to my future girlfriend

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
     $('#title').focus();
       $('#text').autosize();
-  });
+});
+
+
+//https://api.whatsapp.com/send?phone=5577981235945&text=Oi%20lindo%20solteiro%20rs?
+function callMeOnZap() {
+  window.open('https://api.whatsapp.com/send?phone=5577981235945&text=Oi%20lindo%20solteiro%20rs?');
+}
