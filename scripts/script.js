8a2e16d88d3894e39fb0acfaacc26780ca4541ba
Rávilon <ravilon.ads@inf.ufpel.edu.br>
2024-06-23 05:01:41 -0300

    Updatting my site
    Inserting my social networks and projects

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,15 @@ $(document).ready(function(){
 function callMeOnZap() {
   window.open('https://api.whatsapp.com/send?phone=5577981235945&text=Oi%20lindo%20solteiro%20rs?');
 }
+
+function openPopup(url) {
+  // Specify the size and other parameters for the popup window
+  var width = 600;
+  var height = 400;
+  var left = (screen.width - width) / 2;
+  var top = (screen.height - height) / 2;
+  var options = 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left + ',resizable=yes,scrollbars=yes';
+
+  // Open the popup window with the specified URL and options
+  window.open(url, 'PopupWindow', options);
+}
